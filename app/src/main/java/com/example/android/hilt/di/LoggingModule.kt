f4f42fe0c7b345a0605610db4b105e900d26bcef
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-29 16:49:45 +0900

    add the qualifier annotation to avoid ambiguity

--- app/src/main/java/com/example/android/hilt/di/LoggingModule.kt
+++ app/src/main/java/com/example/android/hilt/di/LoggingModule.kt
@@ -9,12 +9,20 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+annotation class InMemoryLogger
+
+@Qualifier
+annotation class DatabaseLogger
 
 @InstallIn(SingletonComponent::class)
 @Module
 abstract class LoggingDatabaseModule {
 
+    @DatabaseLogger
     @Singleton
     @Binds
     abstract fun bindDatabaseLogger(impl: LoggerLocalDataSource): LoggerDataSource
@@ -24,6 +32,7 @@ abstract class LoggingDatabaseModule {
 @Module
 abstract class LoggingInMemoryModule {
 
+    @InMemoryLogger
     @ActivityScoped
     @Binds
     abstract fun bindInMemoryLogger(impl: LoggerInMemoryDataSource): LoggerDataSource
