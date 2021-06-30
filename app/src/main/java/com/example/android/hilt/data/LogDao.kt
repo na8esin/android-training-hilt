e0454b415248f3115029698931d4f4183f44a0fd
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-30 11:20:55 +0900

    LogDao: add methods that return a database Cursor

--- app/src/main/java/com/example/android/hilt/data/LogDao.kt
+++ app/src/main/java/com/example/android/hilt/data/LogDao.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.hilt.data
 
+import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -32,6 +33,14 @@ interface LogDao {
     @Insert
     fun insertAll(vararg logs: Log)
 
+    // nukeはデータを消去すると言う意味らしい
+    // nuclearが由来のスラング
     @Query("DELETE FROM logs")
     fun nukeTable()
+
+    @Query("SELECT * FROM logs ORDER BY id DESC")
+    fun selectAllLogsCursor(): Cursor
+
+    @Query("SELECT * FROM logs WHERE id = :id")
+    fun selectLogById(id: Long): Cursor?
 }
